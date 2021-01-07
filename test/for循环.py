426c1e6c83265976893bfdb7b0f74f444de6fa4f
夏然 <1397757230@qq.com>
2021-01-07 22:00:45 +0800

    Add files via upload

---
+++ test/for循环.py
@@ -0,0 +1,36 @@
+"""循环语句格式
+for 迭代变量 in 对象
+    循环体
+"""
+
+# 实现1到100的相加
+'''result=0
+for i in range(101):
+    result+=i
+print(result)
+'''
+
+# 输出1到100之间的奇数
+'''for i in range(1,100,2):
+    print(i,end=' ')
+'''
+
+# 输出1到100之间的偶数
+'''for i in range(2,100,2):
+    print(i,end=' ')
+'''
+
+# while 循环
+# while 条件表达式
+#    循环体
+'''print("今有物不知数，三三数之剩之，五五数之剩二，七七数之 剩三，问几何？\n")
+none=True
+number=0
+while none:
+    number+=1
+    if number%3==2 and number%5==3 and number%7==2:
+        print("答曰：这个数是",number)
+        none=False
+'''
+
+
